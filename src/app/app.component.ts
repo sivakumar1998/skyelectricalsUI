2d292a1b1c35c772e5e1c77b7fb87bb82cb3d2d9
Burra Siva Kumar <40349298+sivakumar1998@users.noreply.github.com>
2026-01-04 16:08:17 +0530

    added changes made to app folder files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CatalogueComponent } from './catalogue/catalogue.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CatalogueComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
